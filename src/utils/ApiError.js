2e470a40c9655755e6e56ff356f59fdbc5ae8e41
Rahul <rahultaakhr@gmail.com>
2024-05-26 00:31:05 +0530

    user and video model and jwt

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,24 +1,25 @@
-class ApiError extends Error{
+class ApiError extends Error {
     constructor(
         statusCode,
-        mesaage="Something Went wrong",
-        errors=[],
-        stack=''
-    ){
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+
+    ) {
         super(message)
-        this.statusCode=statusCode,
-        this.errors=errors,
-        this.stack=stack,
-        this.data=null,
-        this.success=false
+        this.statusCode = statusCode
+        this.errors = errors
+        this.stack = stack
+        this.data = null
+        this.success = false
 
         if (stack) {
-            this.stack=stack
-        } else {
-            Error.captureStackTrace(this,this.constructor)
+            this.stack = stack
         }
-
+        else {
+            Error.captureStackTrace(this, this.constructor)
+        }
     }
 }
 
-export {ApiError}
+export { ApiError }
