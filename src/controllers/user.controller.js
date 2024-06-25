ce4edc76974fa839cab9fe19a4d52883018262d4
rahultaakhr <rahultaakhr@gmail.com>
2024-06-25 21:54:27 +0530

    aggregation pipeline for user channel profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -307,24 +307,73 @@ const updateAvatar = asyncHandler(async (req, res) => {
     )
 })
 
-const getUserChannel=asyncHandler(async(req,res)=>{
-    const {username}=req.params
-    if(!username.trim()){
-        throw new ApiError(400,"Username not valide")
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params
+    if (!username?.trim()) {
+        throw new ApiError(400, "Invalide user")
+    }
+    const channel = User.aggregate([
+        {
+            $match: {
+                username: username
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount: {
+                    $size: "$subscribers"
+                },
+                channelSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        $if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                email: 1,
+                avatar: 1,
+                subscriberCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1,
+                coverImage: 1
+            }
+        }
+    ])
+    if (!channel?.length) {
+        throw new ApiError(404, "channel does not exist")
 
     }
-   const channel =await User.aggregate([
-    {
-        $match:{
-            username:username?.toLowerCase()
-        }
-    },
-    {
-        $lookup:{
-            from:""
-        }
-    }
-   ])
+    return res
+        .status(200)
+        .json(new ApiResponse(200, channel[0], "User channel fetched successfully"))
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeOldPassword, updateUserDetails, getCurrentUser, updateAvatar }
+export {
+    registerUser, loginUser, logoutUser, refreshAccessToken, changeOldPassword,
+    updateUserDetails, getCurrentUser, updateAvatar, getUserChannelProfile
+}
